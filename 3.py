5e764e083018bed4be1ff70237c513959bb7b140
ismayjuanraro <48245076+ismayjuanraro@users.noreply.github.com>
2019-03-06 14:18:39 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env   pyhton
+# __*__   coding:utf-8  __*__
+def main():
+    print("ORDENAR 3 NUMEROS")
+    lista=[]
+    lista.append(int(input("Escriba un número: "))
+    lista.append(int(input("Escriba otro número: "))
+    lista.append(int(input("Escriba otro número: "))
+    lista.sort()
+    print(lista)
+if __name__ == "__main__":
+    main()
